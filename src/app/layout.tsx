c8233827e59cbf941f9a230d8512d2f52a669dbd
Carlos Agamez <oruz2019@gmail.com>
2026-02-22 00:20:22 -0500

    add ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -23,6 +24,12 @@ export default function RootLayout({
       <body
         className={`${inter.variable} font-sans antialiased bg-[#09090b] text-[#fafafa] min-h-screen flex flex-col`}
       >
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6986304369066538"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         {children}
       </body>
     </html>
